514851d478668995bd81257009ae0bf107ef5309
unknown <phfdea13@gmail.com>
2026-08-22 15:35:00 -0300

    Add binary tree operations: insert, search and list

--- BinaryTree/src/Main.java
+++ BinaryTree/src/Main.java
@@ -1,2 +1,15 @@
+import org.w3c.dom.Node;
+
+import java.util.HashMap;
 public class Main {
-}
+    static HashMap<String, Node> campus = new HashMap<>();
+
+    public static void main(String[] args) {
+        campus.put("Anália Franco", null);
+        campus.put("Guarulhos", null);
+        campus.put("Liberdade", null);
+        campus.put("Paulista", null);
+        campus.put("São Miguel", null);
+        campus.put("Santo Amaro", null);
+        campus.put("Villa Lobos", null);
+    }}
--- BinaryTree/src/Node.java
+++ BinaryTree/src/Node.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Node {
+    String  StundNam;
+    Node left;
+    Node right;
+
+    public Node(String stundNam) {
+        this.StundNam = stundNam;
+        this.left = null;
+        this.right = null;
+    }
 }
+
+
